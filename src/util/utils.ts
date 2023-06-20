f3065b03c1079d5b928985056a88aaae53dd5539
Minn <1056640663@qq.com>
2023-06-20 18:11:35 +0800

    Update utils.ts

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -9,11 +9,3 @@ export function formatToTreeData(list: QuestionItemProps[], parentKey: number | 
   const arr = list.map((v: {question: string, key: number | string}, i: number) => ({title: v.question, key: `${parentKey}-${i}`}));
   return arr;
 }
-
-
-export const getTreeData = (data: any) => {
-  const treeData = Object.entries(data).map(([key, value], index) => {
-    const { title, questionList } = value || {} as any;
-    return { title, belongTo: key, key: `${index}-0`, children: formatToTreeData(questionList, `${index}-0`) }
-  });
-}
