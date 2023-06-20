aff4eabb62b06034a8a17296776195a917c04356
Minn <1056640663@qq.com>
2023-06-20 18:00:41 +0800

    Create ButtonHoc.tsx

---
+++ src/reactTheory/introductions/ButtonHoc.tsx
@@ -0,0 +1,19 @@
+import { memo } from "react";
+import { Button } from "antd";
+import { ICON_ACTIVE_MAP, ICON_MAP } from "./config";
+import { ICON_ADD_TYPE } from "./const";
+import { IconTypeProps } from "./types";
+
+function ButtonHoc({ iconType = ICON_ADD_TYPE, onClickCallback = () => { }, active = false }) {
+  const onClick = (e: any) => {
+    e.stopPropagation();
+    onClickCallback();
+  }
+  // 点击按钮弹出弹窗时，处于激活状态，否则恢复默认
+  const iconMap: Record<string, React.ReactNode> = active ? ICON_ACTIVE_MAP : ICON_MAP;
+  return (
+    <Button type="text" onClick={onClick} size="small" icon={iconMap?.[iconType]} />
+  )
+}
+
+export default memo(ButtonHoc);
