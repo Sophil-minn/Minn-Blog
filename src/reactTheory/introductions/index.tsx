7c6d5e320b821c38d901f7fade2b2caa79d4076c
Minn <1056640663@qq.com>
2023-06-20 18:08:52 +0800

    Update index.tsx

--- src/reactTheory/introductions/index.tsx
+++ src/reactTheory/introductions/index.tsx
@@ -1,8 +1,10 @@
-import { Typography } from 'antd';
+import { Affix, Anchor, Col, Row, Typography } from 'antd';
 import classNames from 'classnames';
 import './index.scss';
 import { ReactTheoryProps } from '../types';
-import Question from './Question';
+import Introduction from './Introduction';
+import { GlobalContext } from '../../App';
+import { useContext } from 'react';
 
 const { Title, Paragraph } = Typography;
 
@@ -10,16 +12,39 @@ const cls = classNames({
   'min-introductions': true
 });
 
-const Introductions = (props: { data: ReactTheoryProps }) => {
-  const { data } = props;
-  const { questionList = [], h2, summary } = data || {};
-  return <div className={cls} >
-    <Title level={2} className='title-2'>{h2}</Title>
-    <Paragraph>{summary}</Paragraph>
-    {
-      questionList.map((v, i) => (<Question data={v} index={i} />))
-    }
-  </div>
+const Introductions = (props: { dataList: ReactTheoryProps[], anchorItems: any }) => {
+  // console.log('data: 2222', props.dataList);
+  const { dataList, anchorItems } = props;
+  const { targetOffset } = useContext(GlobalContext);
+  return (<>
+    {dataList?.map(data => {
+      const { questionList = [], h2, id, summary } = data || {};
+      return data && (
+        <Row wrap={false} gutter={16} key={id}>
+          <Col flex="auto">
+            <div className={cls} >
+              <Title level={2} className='title-2' id={id}>{h2}</Title>
+              <Paragraph>{summary}</Paragraph>
+              {
+                questionList.map((v, i) => (<Introduction data={v} key={v.id} index={i} />))
+              }
+            </div>
+          </Col>
+          <Col flex="200px">
+            <Affix offsetTop={targetOffset}>
+              <Anchor
+                affix={false}
+                targetOffset={targetOffset}
+                // onClick={handleClick}
+                items={anchorItems}
+              />
+            </Affix>
+          </Col>
+        </Row>
+      );
+    })}
+  </>);
+
 }
 
 
