118e98d42f175018994af91aee444b0a471d5b01
Minn <1056640663@qq.com>
2023-06-20 18:07:42 +0800

    Update types.ts

--- src/reactTheory/types.ts
+++ src/reactTheory/types.ts
@@ -1,8 +1,11 @@
 
 export interface TheoryProps {
   title: string;
+  id: string;
   concept: string;
   elements?: string;
+  tips?: string;
+  answer?: string[];
   methods?: {
     title: string;
     content: string;
@@ -12,7 +15,8 @@ export interface TheoryProps {
 
 export interface QuestionItemProps {
   key: string;
-  value: string;
+  title: string;
+  id?: string;
   question: string;
   detail?: {
     description?: string;
@@ -25,8 +29,21 @@ export interface QuestionItemProps {
 
 export  interface ReactTheoryProps {
   h2?: string;
+  id?: string;
   title?: string;
   summary?: string;
   questionList?: QuestionItemProps[]
+}
+
+
+interface AnchorItemsProps {
+  key: string;
+  href: string;
+  title: string;
+  children: {
+    key: string;
+    href: string;
+    title: string;
+  }
 }
 
