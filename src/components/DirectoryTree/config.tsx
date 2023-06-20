6831383790f96c4964d8e48203c8294a5d10e038
Minn <1056640663@qq.com>
2023-06-20 18:01:52 +0800

    Update config.tsx

--- src/components/DirectoryTree/config.tsx
+++ src/components/DirectoryTree/config.tsx
@@ -1,9 +1,22 @@
-import React from "react";
 import {
   SendOutlined,
   FolderOpenOutlined,
-  ApiOutlined
+  ApiOutlined,
+  PlusOutlined,
+  FilterOutlined,
+  PlusSquareFilled,
+  FilterFilled,
+  FileWordOutlined,
+  FileWordFilled,
+  FolderOpenFilled,
+  FolderFilled,
+  FolderOutlined,
+  PlusCircleOutlined,
+  PlusCircleFilled,
+  PlusSquareOutlined
 } from "@ant-design/icons";
+import { ICON_ADD_DIR_TYPE, ICON_ADD_SQUARE_TYPE, ICON_ADD_TYPE, ICON_DIR_TYPE, ICON_FILTER_TYPE, ICON_LEAF_TYPE } from "./const";
+import TreeNode from "./TreeNode";
 
 export const treeData = [
   {
@@ -84,4 +97,80 @@ export const treeData = [
       }
     ]
   }
+];
+
+// 配置icon类型默认线框风格， 
+export const ICON_MAP = {
+  [ICON_ADD_TYPE]: <PlusOutlined />,
+  [ICON_ADD_DIR_TYPE]: <PlusCircleOutlined />,
+  [ICON_ADD_SQUARE_TYPE]: <PlusSquareOutlined />,
+  [ICON_FILTER_TYPE]: <FilterOutlined />,
+  [ICON_DIR_TYPE]: <FolderOutlined />,
+  [ICON_LEAF_TYPE]: <FileWordOutlined />,
+};
+// 激活时icon用对应的实底风格 
+export const ICON_ACTIVE_MAP = {
+  [ICON_ADD_TYPE]: <PlusSquareFilled style={{ color: '#1677ff' }} />,
+  [ICON_ADD_DIR_TYPE]: <PlusCircleFilled style={{ color: '#1677ff' }} />,
+  [ICON_ADD_SQUARE_TYPE]: <PlusSquareFilled style={{ color: '#1677ff' }} />,
+  [ICON_FILTER_TYPE]: <FilterFilled style={{ color: '#1677ff' }} />,
+  [ICON_DIR_TYPE]: <FolderOpenFilled style={{ color: '#1677ff' }} />,
+  [ICON_LEAF_TYPE]: <FileWordFilled style={{ color: '#1677ff' }} />,
+}
+
+
+// 筛选表单配置项
+export const optionsFilter = [
+  {
+    value: "type",
+    label: "类型",
+    type: "select",
+  },
+  {
+    value: "dataSourceCode",
+    label: "系统",
+    type: "select",
+    showSearch: true,
+  },
+  {
+    value: "state",
+    label: "状态",
+    type: "checkbox",
+    options: [
+      {
+        value: "draft",
+        label: "草稿",
+      },
+      {
+        value: "offline",
+        label: "已下线",
+      },
+      {
+        value: "online",
+        label: "已发布",
+      },
+    ],
+  },
+];
+// 新增目录下拉选项
+export const optionsDirectory = [
+  {
+    value: "system",
+    label: "新增目录",
+    type: "select",
+    options: [
+      {
+        value: "draft",
+        label: "目录1",
+      },
+      {
+        value: "offline",
+        label: "目录2",
+      },
+      {
+        value: "online",
+        label: "目录3",
+      },
+    ],
+  },
 ];
