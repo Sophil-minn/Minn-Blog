53e5011590e753ae715f78055cadbbf546f32733
Minn <1056640663@qq.com>
2023-06-26 18:21:36 +0800

    Create config.tsx

---
+++ src/pages/apiPractice/demos/Callback/config.tsx
@@ -0,0 +1,65 @@
+export const anchorItems = [
+  {
+    key: 'Reference',
+    href: '#Reference',
+    title: '1、Reference',
+    children: [
+      {
+        key: '注意事项',
+        href: '#注意事项1',
+        title: '注意事项',
+      },
+    ]
+  },
+  {
+    key: '用法',
+    href: '#用法',
+    title: '2、用法',
+    children: [
+      {
+        key: 'skipping-re-rendering-of-components',
+        href: '#skipping-re-rendering-of-components',
+        title: '2.1、Skipping re-rendering of components',
+        tooltip: true,
+        children: [
+          {
+            key: 'memo example',
+            href: '#memo-example',
+            title: 'memo example',
+          },
+          {
+            key: 'without useCallback',
+            href: '#without-useCallback',
+            title: 'without useCallback',
+          },
+          {
+            key: 'with useCallback',
+            href: '#with-useCallback',
+            title: 'with useCallback',
+          },
+          {
+            key: '注意事项',
+            href: '#注意事项2',
+            title: '注意事项',
+          },
+          {
+            key: '深入探讨2.1',
+            href: '#深入探讨2.1',
+            title: '深入探讨',
+          },
+          {
+            key: '应用示例2.1',
+            href: '#应用示例2.1',
+            title: '应用示例',
+          },
+        ]
+      },
+
+    ],
+  },
+  {
+    key: 'part-3',
+    href: '#part-3',
+    title: 'Part 3',
+  },
+];
