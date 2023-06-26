076f9b9e88d20e92762f342f71eababf89db02ba
Minn <1056640663@qq.com>
2023-06-26 18:25:41 +0800

    Update index.tsx

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,11 +1,13 @@
 import { Skeleton } from 'antd'
+import { SkeletonTitleProps } from 'antd/es/skeleton/Title';
 import React from 'react';
 
 interface LoadingProps {
   loading: boolean;
+  title?: SkeletonTitleProps | undefined | boolean;
   children?: null | undefined | React.ReactNode;
 }
 
-export default function index({loading = false, children = null}: LoadingProps) {
-  return loading ? <Skeleton active /> : <>{children}</>
+export default function index({ loading = false, children = null, title = false }: LoadingProps) {
+  return loading ? <Skeleton title={title} active /> : <>{children}</>
 }
