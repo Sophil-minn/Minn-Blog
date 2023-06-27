082722df3662f6cd52cd3e87e3cb3a3867000e24
Minn <1056640663@qq.com>
2023-06-27 17:20:15 +0800

    Create config.tsx

---
+++ src/pages/apiPractice/demos/Context/config.tsx
@@ -0,0 +1,52 @@
+
+export const contextAnchorItems = [
+  {
+    key: 'useContext',
+    href: '#useContext',
+    title: 'useContext',
+    children: [
+      {
+        key: '参考',
+        href: '#useContext-Refenrence',
+        title: '参考',
+      },
+    ]
+  },
+  {
+    key: 'useContext-Usage',
+    href: '#useContext-Usage',
+    title: '用法',
+    children: [
+      {
+        key: '向组件树深层传递数据',
+        href: '#Passing-data-deeply-into-the-tree',
+        title: '向组件树深层传递数据',
+      },
+      {
+        key: '通过 context 更新传递的数据',
+        href: '#updateDataBycontext',
+        title: '通过 context 更新传递的数据',
+      },
+      {
+        key: '指定回退默认值',
+        href: '#Specifying-a-fallback-default-value',
+        title: '指定回退默认值',
+      },
+      {
+        key: '覆盖组件树一部分的 context ',
+        href: '#overiddingExample',
+        title: '覆盖组件树一部分的 context ',
+      },
+      {
+        key: '在传递对象和函数时优化重新渲染',
+        href: '#Optimizing-re-renders-when-passing-objects-and-functions',
+        title: '在传递对象和函数时优化重新渲染',
+      },
+    ]
+  },
+  {
+    key: 'Troubleshooting',
+    href: '#useContext-Troubleshooting',
+    title: '疑难解答'
+  }
+]
