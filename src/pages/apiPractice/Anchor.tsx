5740796ad7f614176a1b5da527bf8313d3e5c4f3
Minn <1056640663@qq.com>
2023-06-27 17:47:36 +0800

    Create Anchor.tsx

---
+++ src/pages/apiPractice/Anchor.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AffixAncho from '../../components/AffixAncho'
+import { hooksAnchorItems } from './config'
+
+export default function Anchor() {
+  return (
+    <AffixAncho anchorItems={hooksAnchorItems} />
+  )
+}
