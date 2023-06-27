b18e11b17475c6c2f76ac3790f79fc8758ae5797
Minn <1056640663@qq.com>
2023-06-27 17:37:57 +0800

    Update Hooks.tsx

--- src/pages/apiPractice/demos/Hooks.tsx
+++ src/pages/apiPractice/demos/Hooks.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Col, Row, Spin, Typography } from 'antd';
+import CallbackInfo from './Callback/CallbackInfo'
+import ContextInfo from './Context/ContextInfo'
+import Anchor from '../Anchor';
 
 export default function Hooks() {
   return (
-    <div>
-      Hooks
-    </div>
+    <Row wrap={false}>
+      <Col flex="auto">
+        <div>
+          <CallbackInfo />
+          <ContextInfo />
+        </div>
+      </Col>
+      <Col flex="260px"><Anchor /></Col>
+    </Row>
   )
 }
