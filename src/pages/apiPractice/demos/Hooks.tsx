0feabc88094358d30af77cf23aebaeb8836b5c14
Minn <1056640663@qq.com>
2023-06-28 16:40:58 +0800

    Update Hooks.tsx

--- src/pages/apiPractice/demos/Hooks.tsx
+++ src/pages/apiPractice/demos/Hooks.tsx
@@ -3,12 +3,14 @@ import ContextInfo from './Context/ContextInfo'
 import { hooksAnchorItems } from '../config';
 import { withAnchor } from '../../../hoc/withAnchor';
 import DebugValueInfo from './DebugValue/DebugValueInfo';
+import DeferredValueInfo from './DeferredValue/DeferredValueInfo';
 
 const EnhancedComponent = withAnchor(() => (
   <>
     <CallbackInfo />
     <ContextInfo />
     <DebugValueInfo />
+    <DeferredValueInfo />
   </>
 ))
 
