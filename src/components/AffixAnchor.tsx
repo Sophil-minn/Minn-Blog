51c3519e873fe4bac5a3058fb831f35fbd276486
Minn <1056640663@qq.com>
2023-06-28 11:55:34 +0800

    Update AffixAnchor.tsx

--- src/components/AffixAnchor.tsx
+++ src/components/AffixAnchor.tsx
@@ -1,15 +1,15 @@
 import { useContext } from 'react'
 import { GlobalContext } from '../App';
 import { Affix, Anchor, Tooltip } from 'antd';
+import { AnchorItemProps } from '../types';
 
-export default function AffixAnchor({ anchorItems }: { anchorItems: Record<string, any>[] }) {
+export default function AffixAnchor({ anchorItems }: { anchorItems?: AnchorItemProps[] }) {
   const { targetOffset } = useContext(GlobalContext) as any;
-  const loop = (arr: Record<string, any>[]): Record<string, any>[] =>
+  const loop = (arr: AnchorItemProps[]): Record<string, any>[] =>
     (arr || []).map((v: Record<string, any>) =>
       ({ ...v, title: v.tooltip ? <Tooltip title={v.title}>{v.title}</Tooltip> : v.title, children: loop(v.children) })
-      // ({ ...v, title: <Tooltip title={v.title}>{v.title}</Tooltip>, children: loop(v.children) })
     );
-  const items = loop(anchorItems);
+  const items = anchorItems && loop(anchorItems);
   return (
     <Affix offsetTop={targetOffset - 26}>
       <Anchor
