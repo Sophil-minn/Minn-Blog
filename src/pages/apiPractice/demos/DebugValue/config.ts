2dbc2e08f078d5727c890a52e38d56fd310d6308
Minn <1056640663@qq.com>
2023-06-28 11:58:01 +0800

    Create config.ts

---
+++ src/pages/apiPractice/demos/DebugValue/config.ts
@@ -0,0 +1,14 @@
+export const debugValueAnchorItems = [
+  {
+    key: "useDebugValue",
+    href: "#useDebugValue",
+    title: "useDebugValue",
+    children: [
+      {
+        key: "useDebugValueId",
+        href: "#useDebugValueId",
+        title: "用法",
+      },
+    ],
+  },
+];
