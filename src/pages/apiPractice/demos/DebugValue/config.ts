180a548a675d43448dd3bb913e5ff6730fd26a3e
Minn <1056640663@qq.com>
2023-06-28 16:41:37 +0800

    Update config.ts

--- src/pages/apiPractice/demos/DebugValue/config.ts
+++ src/pages/apiPractice/demos/DebugValue/config.ts
@@ -1,13 +1,30 @@
-export const debugValueAnchorItems = [
+export const deferredValueAnchorItems = [
   {
-    key: "useDebugValue",
-    href: "#useDebugValue",
-    title: "useDebugValue",
+    key: "useDeferredValue",
+    href: "#useDeferredValue",
+    title: "useDeferredValue",
     children: [
       {
-        key: "useDebugValueId",
-        href: "#useDebugValueId",
+        key: "useDeferredValueId",
+        href: "#useDeferredValueId",
         title: "用法",
+        children: [
+          {
+            key: "在新内容加载期间显示旧内容",
+            href: "#Showing-stale-content",
+            title: "在新内容加载期间显示旧内容",
+          },
+          {
+            key: "表明内容已过时",
+            href: "#Indicating-that-the-content-is-stale",
+            title: "表明内容已过时",
+          },
+          {
+            key: "延迟渲染 UI 的某些部分",
+            href: "#Deferring-re-rendering-for-a-part-of-the-UI ",
+            title: "延迟渲染 UI 的某些部分",
+          },
+        ],
       },
     ],
   },
