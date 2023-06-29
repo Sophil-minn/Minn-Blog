f09cb61f99a8bfabe9719e3eefb33ea09c8c6474
Minn <1056640663@qq.com>
2023-06-29 17:28:41 +0800

    Update WithoutUseDeferredValue.js

--- src/pages/apiPractice/demos/DeferredValue/Example2/WithoutUseDeferredValue.js
+++ src/pages/apiPractice/demos/DeferredValue/Example2/WithoutUseDeferredValue.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
-import SlowList from "./SlowList";
+import { Suspense, useState, lazy } from "react";
 import { Card } from "antd";
+
+const SlowList = lazy(() => import("./SlowList"));
 
 export default function WithoutUseDeferredValue() {
   const [text, setText] = useState("");
   return (
     <Card title="不使用useDeferredValue渲染">
       <input value={text} onChange={(e) => setText(e.target.value)} />
-      <SlowList text={text} />
+      <Suspense fallback="loading SlowList2 ">
+        <SlowList text={text} />
+      </Suspense>
     </Card>
   );
 }
