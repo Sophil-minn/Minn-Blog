db48a000ad99035e399bd3bdccac635e88c1990b
Minn <1056640663@qq.com>
2023-06-29 17:27:57 +0800

    Update index.js

--- src/pages/apiPractice/demos/DeferredValue/Example2/index.js
+++ src/pages/apiPractice/demos/DeferredValue/Example2/index.js
@@ -1,18 +1,17 @@
-import { useState, useDeferredValue } from "react";
-import SlowList from "./SlowList";
-import WithoutUseDeferredValue from "./WithoutUseDeferredValue";
-import { Card } from "antd";
+import { useState, useDeferredValue, Suspense, lazy } from "react";
+
+const WithoutUseDeferredValue = lazy(() => import("./WithoutUseDeferredValue"));
+const WithUseDeferredValue = lazy(() => import("./WithUseDeferredValue"));
 
 export default function Index() {
-  const [text, setText] = useState("");
-  const deferredText = useDeferredValue(text);
   return (
     <>
-      <Card title="使用useDeferredValue渲染">
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-        <SlowList text={deferredText} />
-      </Card>
-      <WithoutUseDeferredValue />
+      <Suspense fallback="使用useDeferredValue渲染">
+        <WithUseDeferredValue />
+      </Suspense>
+      <Suspense fallback="使用useDeferredValue渲染">
+        <WithoutUseDeferredValue />
+      </Suspense>
     </>
   );
 }
