e49e0d4d53be2ae875a216d46ecc894290e28ce0
Minn <1056640663@qq.com>
2023-06-30 17:30:06 +0800

    Create useChatRoom.js

---
+++ src/pages/apiPractice/demos/Effect/Examples/useChatRoom.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+function createConnection(serverUrl, roomId) {
+  // 真正的实现将实际连接到服务器
+  return {
+    connect() {
+      console.log(
+        '✅ Connecting to "' + roomId + '" room at ' + serverUrl + "..."
+      );
+    },
+    disconnect() {
+      console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
+    },
+  };
+}
+
+export function useChatRoom({ serverUrl, roomId }) {
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+    return () => {
+      connection.disconnect();
+    };
+  }, [roomId, serverUrl]);
+}
