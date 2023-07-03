de12c8c56bb60a205f955b9cc59ac30732c7f074
Minn <1056640663@qq.com>
2023-07-03 17:49:11 +0800

    Update Hooks.tsx

--- src/pages/apiPractice/Hooks.tsx
+++ src/pages/apiPractice/Hooks.tsx
@@ -12,6 +12,13 @@ const EffectInfo = lazy(() => import("./demos/Effect/EffectInfo"));
 const IdInfo = lazy(() => import("./demos/Id/IdInfo"));
 const ImperativeHandleInfo = lazy(() => import("./demos/ImperativeHandle/ImperativeHandleInfo"));
 const InsertionEffectInfo = lazy(() => import("./demos/InsertionEffect/InsertionEffectInfo"));
+const LayoutEffectInfo = lazy(() => import("./demos/LayoutEffect/LayoutEffectInfo"));
+const MemoInfo = lazy(() => import("./demos/Memo/MemoInfo"));
+const ReducerInfo = lazy(() => import("./demos/Reducer/ReducerInfo"));
+const RefInfo = lazy(() => import("./demos/Ref/RefInfo"));
+const StateInfo = lazy(() => import("./demos/State/StateInfo"));
+const SyncExternalStoreInfo = lazy(() => import("./demos/SyncExternalStore/SyncExternalStoreInfo"));
+const TransitionInfo = lazy(() => import("./demos/Transition/TransitionInfo"));
 
 const EnhancedComponent = withAnchor(() => (
   <>
@@ -21,8 +28,15 @@ const EnhancedComponent = withAnchor(() => (
     <SpinLoading tip="Loading useDeferredValue" ><DeferredValueInfo /></SpinLoading>
     <SpinLoading tip="Loading useEffect" ><EffectInfo /></SpinLoading>
     <SpinLoading tip="Loading useId" ><IdInfo /></SpinLoading>
-    <SpinLoading tip="Loading useImperativeHandleInfo" ><ImperativeHandleInfo /></SpinLoading>
-    <SpinLoading tip="Loading InsertionEffectInfo" ><InsertionEffectInfo /></SpinLoading>
+    <SpinLoading tip="Loading useImperativeHandle" ><ImperativeHandleInfo /></SpinLoading>
+    <SpinLoading tip="Loading useInsertionEffect" ><InsertionEffectInfo /></SpinLoading>
+    <SpinLoading tip="Loading useLayoutEffect" ><LayoutEffectInfo /></SpinLoading>
+    <SpinLoading tip="Loading useMemo" ><MemoInfo /></SpinLoading>
+    <SpinLoading tip="Loading useReducer" ><ReducerInfo /></SpinLoading>
+    <SpinLoading tip="Loading useRef" ><RefInfo /></SpinLoading>
+    <SpinLoading tip="Loading useState" ><StateInfo /></SpinLoading>
+    <SpinLoading tip="Loading useSyncExternalStore" ><SyncExternalStoreInfo /></SpinLoading>
+    <SpinLoading tip="Loading useTransition" ><TransitionInfo /></SpinLoading>
   </>
 ))
 
