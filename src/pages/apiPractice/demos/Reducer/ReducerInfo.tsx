5fb1885610d6a52ae6bd5f0069cdf0aec086fbe1
Minn <1056640663@qq.com>
2023-07-03 17:51:28 +0800

    Create ReducerInfo.tsx

---
+++ src/pages/apiPractice/demos/Reducer/ReducerInfo.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import { Card, Typography } from 'antd';
+import { lazy } from 'react'
+// import { code1, } from './code';
+
+const SimpleCode = lazy(() => import('../../../../components/SimpleCode'));
+const { Title, Paragraph, Text } = Typography;
+
+export default function ReducerInfo() {
+  return (
+    <div>ReducerInfo</div>
+  )
+}
+
+
