a220d9430cd7d0731e7e5dea9d7b1a2380c7c3d3
Minn <1056640663@qq.com>
2023-07-05 17:52:23 +0800

    Update config.tsx

--- src/pages/engineering/config.tsx
+++ src/pages/engineering/config.tsx
@@ -1,36 +1,44 @@
-import MenuLabel from '../../components/MenuLabel';
+import { getMenuItem } from '../../util/config';
+import { MenuItem } from '../apiPractice/types';
 
 // submenu keys of first level
 export const rootSubmenuKeys = ['sub11', 'sub2', 'sub4'];
 
-interface MenuItem {
-  label: string | React.ReactNode;
-  key: string;
-  icon?: React.ReactNode;
-  children?: MenuItem[];
-  type?: 'group';
-  title?: string;
-}
-
-function getItem(
-  label: string,
-  path: string,
-  key: string,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: 'group',
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label: <MenuLabel keypath={key} path={path} label={label} />,
-    type,
-    path
-  } as MenuItem;
-}
 
 export const items: MenuItem[] = [
-  getItem('A', 'A', '1'),
-  getItem('B', 'B', '2'),
+  getMenuItem('前端工程化', '/engineering', 'engineering', [
+    getMenuItem('前端模块化', '/engineering/modularization', 'modularization'),
+    getMenuItem('前端组件化', '/engineering/componentization', 'componentization'),
+  ]),
+];
+
+
+export const engineeringAnchorItems = [
+  {
+    key: "theory",
+    href: "#theory",
+    title: "概念",
+  },
+  {
+    key: "why need engineering",
+    href: "#why-need-engineering",
+    title: "为什么需要工程化",
+    children: [
+      {
+        key: "Efficiency",
+        href: "#Efficiency",
+        title: "提升开发效率",
+      },
+      {
+        key: "performance",
+        href: "#performance",
+        title: "优化性能",
+      },
+      {
+        key: "qulity",
+        href: "#qulity",
+        title: "提高代码质量",
+      },
+    ],
+  },
 ];
