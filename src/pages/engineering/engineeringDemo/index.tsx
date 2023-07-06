f0ad763de452e734c568436723228e51a72bd974
Minn <1056640663@qq.com>
2023-07-06 17:55:48 +0800

    Update index.tsx

--- src/pages/engineering/engineeringDemo/index.tsx
+++ src/pages/engineering/engineeringDemo/index.tsx
@@ -1,15 +1,18 @@
 import { useParams } from 'react-router-dom';
 import { componentsMap } from './config';
 import { RouteParams } from './types';
-import Engineering from '..';
+import { useEffect } from 'react';
 
 
 export default function EngineeringDemo() {
   const { id } = useParams() as unknown as RouteParams;
   const Comp = id ? componentsMap?.[id] || undefined : undefined;
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [id])
   return (
     <div>
-      {Comp ? <Comp /> : <Engineering />}
+      {Comp ? <Comp /> : null}
     </div>
   )
 }
