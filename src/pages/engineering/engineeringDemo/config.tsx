90b8e32333f06599e0ee5562994ff1da136b14e7
Minn <1056640663@qq.com>
2023-07-06 17:55:33 +0800

    Update config.tsx

--- src/pages/engineering/engineeringDemo/config.tsx
+++ src/pages/engineering/engineeringDemo/config.tsx
@@ -1,9 +1,11 @@
 import Modularization from "../modularization";
 import Componentization from "../componentization";
 import { ComponentsMap } from "./types";
+import ModularizationHistory from "../modularization/ModularizationHistory";
 
 
 export const componentsMap: ComponentsMap = {
   modularization: () => <Modularization />,
+  modularizationHistory: () => <ModularizationHistory />,
   componentization: () => <Componentization />,
 };
