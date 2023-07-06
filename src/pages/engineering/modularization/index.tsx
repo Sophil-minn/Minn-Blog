f8a3eb2b21f2306064e31fc857f4ad91b78e9618
Minn <1056640663@qq.com>
2023-07-06 18:02:43 +0800

    Update index.tsx

--- src/pages/engineering/modularization/index.tsx
+++ src/pages/engineering/modularization/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { withAnchor } from '../../../hoc/withAnchor';
+import { modularizationAnchorItems } from '../config';
+import Info from './Info';
+import './index.scss';
 
-export default function Componentization() {
-  return (
-    <div>Componentization</div>
-  )
+const EnhancedComponent = withAnchor(Info)
+
+export default function Modularization() {
+  return <EnhancedComponent anchorItems={modularizationAnchorItems} />;
 }
-
-
