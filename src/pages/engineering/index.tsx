5de6d6c5d45c1d483ad681318f9e9bccd5f75e0e
Minn <1056640663@qq.com>
2023-07-06 17:55:10 +0800

    Update index.tsx

--- src/pages/engineering/index.tsx
+++ src/pages/engineering/index.tsx
@@ -2,10 +2,15 @@ import { items } from './config';
 import WebEngineering from './WebEngineering';
 import withMenu from '../../hoc/withMenu';
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import './index.scss';
 
 const EnhancedComponent = withMenu(<WebEngineering />);
 
 export default function Engineering() {
   const { id } = useParams() as { id: string };
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [id])
   return <EnhancedComponent items={items} id={id} defaultOpenKeys={['engineering']} />;
 }
