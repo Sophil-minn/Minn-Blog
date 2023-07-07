6c6605252d60c07d629f5078da7a2e0dd8e381fa
Minn <1056640663@qq.com>
2023-07-07 17:32:56 +0800

    Update index.tsx

--- src/pages/componentsDevelopment/index.tsx
+++ src/pages/componentsDevelopment/index.tsx
@@ -1,53 +1,33 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-
+import { items } from './config';
+import withMenu from '../../hoc/withMenu';
+import { Outlet, useOutlet, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import './index.scss';
-import Container from '../../block/container';
-import { Card, Col, Menu, MenuProps, Row } from 'antd';
-import { Outlet } from 'react-router-dom';
-import { items, rootSubmenuKeys } from './config';
+import LibaryDev from './demos/LibaryDev';
+import DevEnviroment from './demos/DevEnviroment';
+import Enginering from './demos/Enginering';
+import ComponentTheory from './demos/ComponentTheory';
+import ComponentSettleDown from './demos/ComponentSettleDown';
 
-const cls = classNames({
-  'minn-components-development': true
-});
+export default function Engineering() {
+  const outlet = useOutlet();
+  const { id } = useParams() as { id: string };
+  let component = (<>
+    <LibaryDev />
+    <DevEnviroment />
+    <Enginering />
+    <ComponentTheory />
+    <ComponentSettleDown />
+  </>)
 
-const ComponentsDevelopment = () => {
-  const [openKeys, setOpenKeys] = useState(['sub1']);
-  const onOpenChange: MenuProps['onOpenChange'] = (keys) => {
-    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-    if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
-
-  const onMenuItemClick = ({ item, key, keyPath }: Record<string, any>) => {
-    // console.log('item, key, keyPath: ', item, key, keyPath);
-
+  if (outlet) {
+    component = <Outlet />;
   }
 
-  return <Container cls={cls}>
-    <Row gutter={8}>
-      <Col flex="260px">
-        <Card>
-          <Menu
-            mode="inline"
-            openKeys={openKeys}
-            onOpenChange={onOpenChange}
-            style={{ width: 256 }}
-            items={items}
-            onClick={onMenuItemClick}
-          />
-        </Card>
-      </Col>
-      <Col flex="auto">
-        <Card>
-          <Outlet />
-        </Card>
-      </Col>
-    </Row>
-  </Container >
-}
+  const EnhancedComponent = withMenu(component);
 
-export default ComponentsDevelopment; 
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [id])
+  return <EnhancedComponent items={items} id={id} defaultOpenKeys={['engineering']} />;
+}
