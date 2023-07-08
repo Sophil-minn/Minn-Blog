366522ca9bc0b2ace9c93a54651c9bf83bae3796
huimin.yang <1056640663@qq.com>
2023-07-08 23:43:58 +0800

    eidt

--- src/pages/react/managingState/config.tsx
+++ src/pages/react/managingState/config.tsx
@@ -31,14 +31,14 @@ function getItem(
 }
 
 export const items: MenuItem[] = [
-  getItem('状态管理', 'managingState', 'managingState', null, [
-    getItem('使用状态响应输入', 'managingState/reacting-to-input-with-state', '1'),
-    getItem('选择状态结构 ', 'managingState/choosing-the-state-structure', '2'),
-    getItem('在组件间共享状态', 'managingState/sharing-state-between-components ', '3'),
-    getItem('保留和重置状态', 'managingState/preserving-and-resetting-state', '5'),
-    getItem('提取状态逻辑到 reducer 中', 'managingState/extracting-state-logic-into-a-reducer', '6'),
-    getItem('使用 Context 进行深层数据传递', 'managingState/passing-data-deeply-with-context', '7'),
-    getItem('使用 Reducer 和 Context 进行状态扩展', 'managingState/scaling-up-with-reducer-and-context', '8'),
+  getItem('状态管理', '', 'managing-state', null, [
+    getItem('使用状态响应输入', '/react/managing-state/reacting-to-input-with-state', '1'),
+    getItem('选择状态结构 ', '/react/managing-state/choosing-the-state-structure', '2'),
+    getItem('在组件间共享状态', '/react/managing-state/sharing-state-between-components ', '3'),
+    getItem('保留和重置状态', '/react/managing-state/preserving-and-resetting-state', '5'),
+    getItem('提取状态逻辑到 reducer 中', '/react/managing-state/extracting-state-logic-into-a-reducer', '6'),
+    getItem('使用 Context 进行深层数据传递', '/react/managing-state/passing-data-deeply-with-context', '7'),
+    getItem('使用 Reducer 和 Context 进行状态扩展', '/react/managing-state/scaling-up-with-reducer-and-context', '8'),
   ]),
 ];
 
--- src/pages/react/managingState/demos/Demo.tsx
+++ src/pages/react/managingState/demos/Demo.tsx
@@ -1,19 +1,16 @@
-import {  useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { componentsMap, rComponentsMap } from './config';
 import { CHOOSING_THE_STATE_STRUCTURE, EXTRACTING_STATE_INTO_A_REDUCER, MANAGING_STATE, PASSING_DATA_DEEPLY_WITH_CONTEXT, PRESERVING_AND_RESETTING_STATE, REACTING_TO_INPUT_WITH_STATE, SCALING_UP_WITH_REDUCER_ADN_CONTEXT, SHARING_STATE_BETWEEN_COMPONENTS, } from "./const";
+import ManagingState from './ManagingState';
 
 interface RouteParams {
-  pid: typeof MANAGING_STATE,
-  id: typeof REACTING_TO_INPUT_WITH_STATE | typeof CHOOSING_THE_STATE_STRUCTURE | typeof SHARING_STATE_BETWEEN_COMPONENTS | typeof PRESERVING_AND_RESETTING_STATE | typeof EXTRACTING_STATE_INTO_A_REDUCER | typeof PASSING_DATA_DEEPLY_WITH_CONTEXT | typeof SCALING_UP_WITH_REDUCER_ADN_CONTEXT
+  id: typeof MANAGING_STATE,
+  pid: typeof REACTING_TO_INPUT_WITH_STATE | typeof CHOOSING_THE_STATE_STRUCTURE | typeof SHARING_STATE_BETWEEN_COMPONENTS | typeof PRESERVING_AND_RESETTING_STATE | typeof EXTRACTING_STATE_INTO_A_REDUCER | typeof PASSING_DATA_DEEPLY_WITH_CONTEXT | typeof SCALING_UP_WITH_REDUCER_ADN_CONTEXT
 }
 export default function Demo() {
   // root组件 匹配
   const { pid, id } = useParams() as unknown as RouteParams;
-  const Comp = pid ? rComponentsMap?.[pid] || undefined : undefined;
-  const CComp = id ? componentsMap?.[id] || undefined : undefined;
-  return (
-    <div>
-      {CComp ? <CComp /> : (Comp ? < Comp /> : null)}
-    </div>
-  )
+  const Comp = pid ? componentsMap?.[pid] || ManagingState : ManagingState;
+  // const CComp = id ? componentsMap?.[id] || undefined : undefined;
+  return <Comp />
 }
