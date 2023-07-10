cd755d1dade609aee2640ee58c6b4d2157d5b755
Minn <1056640663@qq.com>
2023-07-10 21:35:04 +0800

    Update Demo.tsx

--- src/pages/designPattern/Demo.tsx
+++ src/pages/designPattern/Demo.tsx
@@ -1,31 +1,20 @@
 import { useParams } from 'react-router-dom';
 import { componentsMap } from './config';
 import {
-  LAW_OF_DEMETER, DESIGN_RULE, COMMON_PATTERNS, UNCOMMON_PATTERNS,
-  OBSERVER_PATTERN, DECORATOR_PATTERN, ADAPTEE_PATTERN, FACTORY_PATTERN, SINGLE_PATTERN, PROXY_PATTERN, FACADE_PATTERN, ITERATOR_PATTERN,
-  INTERPRETER_PATTERN, MEDIATOR_PATTERN, VISITOR_PATTERN, MEME_PATTERN, COMMAND_PATTERN,
-  ACTION_PATTERN, TEMPLATE_METHOD_PATTERN, STRATEGY_PATTERN, FLYWEIGHT_PATTERN, COMBINATION, BRIDGE_PATTERN,
-  PROTOTYPE_PATTERN
+  LAW_OF_DEMETER, DESIGN_RULE, COMMON_PATTERNS, UNCOMMON_PATTERNS
 } from './const';
 
 interface RouteParams {
   id: typeof LAW_OF_DEMETER | typeof DESIGN_RULE | typeof COMMON_PATTERNS | typeof UNCOMMON_PATTERNS
 }
-interface RouteParams2 {
-  id: typeof OBSERVER_PATTERN | typeof DECORATOR_PATTERN | typeof ADAPTEE_PATTERN | typeof FACTORY_PATTERN | typeof SINGLE_PATTERN | typeof PROXY_PATTERN | typeof FACADE_PATTERN | typeof ITERATOR_PATTERN
-}
-interface RouteParams3 {
-  id: typeof INTERPRETER_PATTERN | typeof MEDIATOR_PATTERN | typeof VISITOR_PATTERN | typeof MEME_PATTERN | typeof COMMAND_PATTERN | typeof
-  ACTION_PATTERN | typeof TEMPLATE_METHOD_PATTERN | typeof STRATEGY_PATTERN | typeof FLYWEIGHT_PATTERN | typeof COMBINATION | typeof BRIDGE_PATTERN | typeof
-  PROTOTYPE_PATTERN
-}
+
 
 export default function CDemo() {
   const { id } = useParams() as unknown as RouteParams;
   const Comp = id ? componentsMap?.[id] || undefined : undefined;
   return (
     <div>
-      {Comp ? <Comp /> : <div>前端打包</div>}
+      {Comp}
     </div>
   )
 }
