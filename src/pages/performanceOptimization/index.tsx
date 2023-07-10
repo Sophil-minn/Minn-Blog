b03efcd769821e3d8a64287a8fc1a84e9b9c8a0d
Minn <1056640663@qq.com>
2023-07-10 21:36:56 +0800

    Update index.tsx

--- src/pages/performanceOptimization/index.tsx
+++ src/pages/performanceOptimization/index.tsx
@@ -1,56 +1,16 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-
+import { items } from './config';
+import PerformanceOptimization from './PerformanceOptimization';
+import withMenu from '../../hoc/withMenu';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import './index.scss';
-import Container from '../../block/container';
-import { Card, Col, Menu, MenuProps, Row } from 'antd';
-import { Outlet } from 'react-router-dom';
-import { items, rootSubmenuKeys } from './config';
 
-const cls = classNames({
-  'minn-components-development': true
-});
+const EnhancedComponent = withMenu(<PerformanceOptimization />);
 
-const ComponentsDevelopment = () => {
-  const [openKeys, setOpenKeys] = useState(['sub1']);
-  const onOpenChange: MenuProps['onOpenChange'] = (keys) => {
-    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-    if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
-
-  const onMenuItemClick = ({ item, key, keyPath }: Record<string, any>) => {
-    // console.log('item, key, keyPath: ', item, key, keyPath);
-
-  }
-
-  // const onTitleClick = ({ key }: any) => {
-  //   console.log('key: ', key);
-
-  // }
-
-  return <Container cls={cls}>
-    <Row gutter={8}>
-      <Col flex="260px">
-        <Card>
-          <Menu
-            mode="inline"
-            openKeys={openKeys}
-            onOpenChange={onOpenChange}
-            style={{ width: 256 }}
-            items={items}
-            onClick={onMenuItemClick}
-          />
-        </Card>
-      </Col>
-      <Col flex="auto">
-        <Card><Outlet /></Card>
-      </Col>
-    </Row>
-  </Container >
+export default function Engineering() {
+  const { id } = useParams() as { id: string };
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [id])
+  return <EnhancedComponent items={items} id={id} />;
 }
-
-export default ComponentsDevelopment; 
